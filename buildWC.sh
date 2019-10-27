6190e48824f0e84eb9f0f476813b9bec50fe53d1
Vincent Euloge <vincent.euloge@gmail.com>
2019-10-27 20:42:33 +0100

    doc: explain what you can find in this repo

--- buildWC.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-npm run build ngelements -- --prod --output-hashing=none --single-bundle true --keep-polyfills true
-mv dist/ngelements/main.js demo/ngelements.js
-mv dist/ngelements/polyfills.js demo
